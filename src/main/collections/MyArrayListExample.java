d54a749fe84ea5e107257127e70c22aa1f6e5779
Melekhin <s.melekhin@bftcom.com>
2021-12-07 09:33:23 +0300

    example arrayList part1

---
+++ src/main/collections/MyArrayListExample.java
@@ -0,0 +1,104 @@
+package main.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author s.melekhin
+ * @since 03 дек. 2021 г.
+ */
+public class MyArrayListExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayListString = new ArrayList<>();
+        arrayListString.add("Ivan");
+        arrayListString.add("Stanislav");
+        arrayListString.add("Elena");
+        arrayListString.add(1, "Misha");
+        System.out.println(arrayListString);
+
+        arrayListString.set(0, "Anton");
+
+        System.out.println(arrayListString);
+
+        arrayListString.remove(0);
+
+        System.out.println(arrayListString);
+
+        ArrayList<String> arrayListWithInitCapacity = new ArrayList<>(200);
+
+        List<String> arrayListInterface = new ArrayList<>();
+
+        List<String> arrayListWithList = new ArrayList<>(arrayListString);
+        System.out.println(arrayListString == arrayListWithList);
+
+        //Student
+        Student st1 = new Student("Stanislav", 'm', 33, 1, 10);
+        Student st2 = new Student("Elena", 'w', 23, 1, 9);
+        Student st3 = new Student("Oleg", 'm', 35, 1, 8);
+        Student st4 = new Student("Dmitriy", 'm', 43, 1, 7.2);
+        Student st5 = new Student("Ivan", 'm', 27, 1, 2);
+
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(st1);
+        studentList.add(st2);
+        studentList.add(st3);
+        studentList.add(st4);
+        studentList.add(st5);
+        System.out.println(studentList);
+
+        Student st6 = new Student("Ivan", 'm', 27, 1, 2);
+
+        studentList.remove(st6);
+
+        System.out.println(studentList);
+
+    }
+
+}
+
+class Student {
+
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return sex == student.sex && age == student.age && course == student.course && Double.compare(student.avgGrade, avgGrade) == 0
+                && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sex, age, course, avgGrade);
+    }
+}
