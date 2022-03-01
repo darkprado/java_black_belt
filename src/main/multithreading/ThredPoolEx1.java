caf228e2d4973103da06360565384eb78ff61859
Melekhin <s.melekhin@bftcom.com>
2022-03-01 10:04:58 +0300

    executorService

---
+++ src/main/multithreading/ThredPoolEx1.java
@@ -0,0 +1,36 @@
+package main.multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author s.melekhin
+ * @since 01 март 2022 г.
+ */
+public class ThredPoolEx1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new RunnableImpl());
+        }
+        executorService.shutdown();
+//        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        System.out.println("main ends");
+    }
+
+}
+
+class RunnableImpl implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
